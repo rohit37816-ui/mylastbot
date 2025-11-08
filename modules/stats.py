d6928f8f4bcc24d94b44fd4e2d02df30c508e35e
rohit37816-ui <rohit37816@gmail.com>
2025-11-08 13:05:03 +0530

    Create stats.py

---
+++ modules/stats.py
@@ -0,0 +1,7 @@
+def get_user_stats(user_id: int):
+    # TODO: Return stats like sections count, favorites, trash count
+    pass
+
+def get_global_stats():
+    # TODO: Return aggregated stats for all users
+    pass
