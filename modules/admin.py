48f97afd5941f73492171fbc79697c001b415716
rohit37816-ui <rohit37816@gmail.com>
2025-11-08 13:04:45 +0530

    Create admin.py

---
+++ modules/admin.py
@@ -0,0 +1,11 @@
+def list_all_users():
+    # TODO: Return all registered users
+    pass
+
+def delete_user_data(username: str):
+    # TODO: Delete data for a specific user
+    pass
+
+def backup_all_data():
+    # TODO: Backup entire bot data for admin
+    pass
