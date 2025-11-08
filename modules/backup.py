42bd83848bbe29e3b6fcc3b61060f37d009e2566
rohit37816-ui <rohit37816@gmail.com>
2025-11-08 13:04:20 +0530

    Create backup.py

---
+++ modules/backup.py
@@ -0,0 +1,7 @@
+def backup_user_data(user_id: int):
+    # TODO: Export user data to backup file
+    pass
+
+def restore_user_data(user_id: int, backup_file_path: str):
+    # TODO: Restore user data from backup file
+    pass
