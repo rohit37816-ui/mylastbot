7b4b930f35071fbf0ee5d0c082bd947a61d4e4a7
rohit37816-ui <rohit37816@gmail.com>
2025-11-08 13:05:25 +0530

    Create settings.py

---
+++ modules/settings.py
@@ -0,0 +1,7 @@
+def get_user_settings(user_id: int):
+    # TODO: Load user's settings from their JSON file
+    pass
+
+def save_user_settings(user_id: int, settings: dict):
+    # TODO: Save settings to user's JSON file
+    pass
