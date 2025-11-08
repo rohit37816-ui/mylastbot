b72b9b039975cebbadcf0820d8e2c3ae47f90a23
rohit37816-ui <rohit37816@gmail.com>
2025-11-08 13:04:01 +0530

    Create trash.py

---
+++ modules/trash.py
@@ -0,0 +1,11 @@
+def list_trash(user_id: int):
+    # TODO: Return sections marked as deleted
+    pass
+
+def restore_from_trash(user_id: int, section_id: str):
+    # TODO: Restore a trashed section
+    pass
+
+def empty_trash(user_id: int):
+    # TODO: Permanently delete all trash sections
+    pass
